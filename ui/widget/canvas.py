0af86e3c05a631a4a1d1f2e8b9fc8815ad4165fa
Chongju Mai <maichongju@hotmail.com>
2022-10-20 13:29:17 -0400

    Update Canvas implementation. Added new plot

--- ui/widget/canvas.py
+++ ui/widget/canvas.py
@@ -1,24 +1,28 @@
 from __future__ import annotations
 
 import time
-from log.logger import LOGGER
-
 from typing import List
 
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel, QHBoxLayout
-from PyQt6.QtCore import Qt
-
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
+from general.enum import Enum, auto
 from general.utils import ensure_folder_exist
+from log.logger import LOGGER
 
 EXPORT_FOLDER = 'export'
+
+
+class FigureType(Enum):
+    SCATTER = auto()
+    PLOT = auto()
 
 
 class CanvasWidget(QWidget):
     def __init__(self, x_label: str,
                  y_label: str,
+                 figure_type: FigureType,
                  title: str = None,
                  invert_x: bool = False,
                  invert_y: bool = False,
@@ -26,17 +30,19 @@ class CanvasWidget(QWidget):
         super().__init__(parent=parent)
         main_layout = QVBoxLayout()
         self.setLayout(main_layout)
-        main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        
+        # main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
         lbl_layout = QHBoxLayout()
         main_layout.addLayout(lbl_layout)
         lbl_layout.addStretch()
         lbl_title = QLabel(title)
         lbl_layout.addWidget(lbl_title)
         lbl_layout.addStretch()
-        
-        
-        self.canvas = Canvas2D(x_label, y_label, invert_x, invert_y, title)
+
+        if figure_type == FigureType.SCATTER:
+            self.canvas = Canvas2DScatter(x_label, y_label, invert_x, invert_y, title)
+        else:
+            self.canvas = Canvas2DPlot(x_label, y_label, title)
         main_layout.addWidget(self.canvas)
 
         btn_layout = QHBoxLayout()
@@ -46,12 +52,12 @@ class CanvasWidget(QWidget):
         btn_save = QPushButton("Save")
         btn_save.clicked.connect(self.btn_save_clicked)
         btn_layout.addWidget(btn_save)
-        
+
         btn_layout.addStretch()
-        
+
         self._file_name = None
 
-    def plot(self, points: List, file_name: str):
+    def plot(self, points: List | dict, file_name: str):
         self._file_name = file_name
         self.canvas.plot(points)
 
@@ -59,60 +65,116 @@ class CanvasWidget(QWidget):
         if self._file_name is None or self._file_name == '':
             return
         self.canvas.save(self._file_name)
-        
+
     def clear(self):
         self.canvas.clear()
 
 
 class Canvas2D(FigureCanvas):
-    def __init__(self, 
-                 x_label: str, 
-                 y_label: str, 
-                 invert_x: bool = False, 
-                 invert_y: bool = False,
+    def __init__(self,
+                 x_label: str,
+                 y_label: str,
                  title: str = None):
-        self.fig = Figure(figsize=(5,4))
+        self.fig = Figure(figsize=(6, 3))
         super().__init__(self.fig)
+
         self.ax = self.fig.add_subplot(111)
-        
-        if invert_x:
-            self.ax.invert_xaxis()
-        if invert_y:
-            self.ax.invert_yaxis()
-        
+
         self.ax.set_xlabel(x_label)
         self.ax.set_ylabel(y_label)
         self.x_label = x_label
         self.y_label = y_label
         self.title = title
+        self.fig.tight_layout()
+        self.ax.grid(axis='both')
 
-    def plot(self, points):
-        self.ax.clear()
-        size = len(points[0])
-        for i in range(size):
-            self.ax.scatter(points[0][i], points[1][i], c='#5895fc')
+    def plot(self, points: dict | List):
+        self.ax.cla()
         self.ax.set_xlabel(self.x_label)
         self.ax.set_ylabel(self.y_label)
-        self.draw()
+        self.ax.set_title(self.title)
+        self.ax.grid(axis='both')
 
-    def save(self, source_file_name):
+    def save(self, source_file_name: str):
         if source_file_name is None:
             return
         ensure_folder_exist(EXPORT_FOLDER)
         if not self.title:
             self.title = f'{self.x_label}{self.y_label}'
-            
+
         file_name = f'{EXPORT_FOLDER}/{source_file_name}_{self.title}_{time.time()}.png'
         self.fig.savefig(file_name)
         LOGGER.info(f'Saved to {file_name}')
 
     def clear(self):
-        self.ax.clear()
+        self.ax.cla()
+        self.draw()
+
+    def draw(self):
+        self.fig.tight_layout()
+        super().draw()
+
+
+class Canvas2DPlot(Canvas2D):
+    def __init__(self,
+                 x_label: str,
+                 y_label: str,
+                 title: str = None):
+        super().__init__(x_label, y_label, title)
+
+    def plot(self, points: dict, legend: str = None):
+        """
+        Plot the 2d points with the given data points. Pass dist when need more than one
+        set of data with the label as the key. then use show legend to decide whether to
+        show the legend or not.
+
+        Args:
+            points: the points to plot
+            legend: the legend location. see https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.legend.html for
+            more info
+        """
+        super().plot(points)
+        if not isinstance(points, dict):
+            raise ValueError(f'points must be a list, not {type(points)}')
+
+        for label, value in points.items():  # type: str, dict
+            if not isinstance(value, dict):
+                raise ValueError(f'points must be a dict, not {type(value)}')
+
+            if 'x' not in value:
+                self.ax.plot(value['y'], label=label)
+            elif 'x' in value and 'y' in value:
+                self.ax.plot(value['x'], value['y'], label=label)
+
+        if legend is not None:
+            self.ax.legend(loc=legend)
+        self.draw()
+
+
+class Canvas2DScatter(Canvas2D):
+    def __init__(self,
+                 x_label: str,
+                 y_label: str,
+                 invert_x: bool = False,
+                 invert_y: bool = False,
+                 title: str = None):
+        super().__init__(x_label, y_label, title)
+
+        if invert_x:
+            self.ax.invert_xaxis()
+        if invert_y:
+            self.ax.invert_yaxis()
+
+    def plot(self, points: List):
+        super().plot(points)
+        size = len(points[0])
+        for i in range(size):
+            self.ax.scatter(points[0][i], points[1][i], c='#5895fc')
         self.draw()
 
 
 class Canvas3D(FigureCanvas):
-    def __init__(self,):
+    def __init__(self, ):
         self.fig = Figure(figsize=(8, 8))
         super().__init__(self.fig)
         self.ax = self.fig.add_subplot(111, projection='3d')
--- ui/widget/tab/twodplottab.py
+++ ui/widget/tab/twodplottab.py
@@ -1,55 +1,78 @@
 import pandas as pd
-from ast import main
-from ui.widget.tab.tab import Tab
-from PyQt6.QtWidgets import QGridLayout, QWidget, QScrollArea, QHBoxLayout
 from PyQt6.QtCore import Qt
-from ui.widget.canvas import CanvasWidget
+from PyQt6.QtWidgets import QGridLayout, QWidget, QScrollArea, QHBoxLayout
+
+from ui.widget.canvas import CanvasWidget, FigureType
+from ui.widget.tab.tab import Tab
 
 
 class TwoDPlotTab(Tab):
     def __init__(self, parent=None):
         super().__init__('2D Plot', parent=parent)
 
-        widget = QWidget() # Dummy widget to hold the layout
+        self._canvas = []
+
+        widget = QWidget()  # Dummy widget to hold the layout
         main_layout = QGridLayout(self)
         widget.setLayout(main_layout)
-        
-        
+
         dummy_layout = QHBoxLayout()
         scroll = QScrollArea()
         scroll.setWidget(widget)
         scroll.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
         scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         scroll.setWidgetResizable(True)
-        
+
         dummy_layout.addWidget(scroll)
         self.setLayout(dummy_layout)
-        
-        
-        self.height_canvas = CanvasWidget('time', 'height', title='Height')
+
+        self.height_canvas = CanvasWidget(x_label='', y_label='different', figure_type=FigureType.PLOT,
+                                          title='Height Stable')
         main_layout.addWidget(self.height_canvas, 0, 0)
 
-        self.xy_top_canvas = CanvasWidget('y', 'x', title='Top View', invert_x=True)
+        self.xy_top_canvas = CanvasWidget(x_label='y', y_label='x', figure_type=FigureType.SCATTER, title='Top View',
+                                          invert_x=True)
         main_layout.addWidget(self.xy_top_canvas, 0, 1)
-        
+        self.xy_stable_canvas = CanvasWidget(x_label='', y_label='difference', figure_type=FigureType.PLOT,
+                                             title='XY Stable')
+        main_layout.addWidget(self.xy_stable_canvas, 1, 0)
+
+        self._canvas.append(self.height_canvas)
+        self._canvas.append(self.xy_top_canvas)
+        self._canvas.append(self.xy_stable_canvas)
+
     def plot(self, df: pd.DataFrame, file_name: str):
-        
         size = len(df.index)
-        
+
         # Height plot
-        height_data = [[i for i in range(size)], df['stateEstimate.z'].to_list()]
-        self.height_canvas.plot(height_data, file_name=file_name)
-        
+        height_data = df['stateEstimate.z'].to_list()
+        hover_data = df['hover.z'].to_list()
+        diff_z = []
+        for hover_height, z in zip(height_data, hover_data):
+            diff_z.append(0 if pd.isna(hover_height) else hover_height - z)
+        draw_data = {'height': {'y': diff_z}}
+
+        self.height_canvas.plot(draw_data, file_name=file_name)
+
         # Top View Plot
         top_view_data = [df['stateEstimate.y'].to_list(), df['stateEstimate.x'].to_list()]
         self.xy_top_canvas.plot(top_view_data, file_name=file_name)
-        
+
+        # XY Stable Plot
+        xy_stable_data = {}
+        hover_x = df['hover.x'].to_list()
+        hover_y = df['hover.y'].to_list()
+        pos_x = df['stateEstimate.x'].to_list()
+        pos_y = df['stateEstimate.y'].to_list()
+        diff_x = []
+        diff_y = []
+        for hx, px, hy, py in zip(hover_x, pos_x, hover_y, pos_y):
+            diff_x.append(0 if pd.isna(hx) else hx - px)
+            diff_y.append(0 if pd.isna(hy) else hy - py)
+        xy_stable_data['x'] = {'y': diff_x}
+        xy_stable_data['y'] = {'y': diff_y}
+
+        self.xy_stable_canvas.plot(xy_stable_data, file_name=file_name)
+
     def clear(self):
-        self.height_canvas.clear()
-        self.xy_top_canvas.clear()     
-    
-        
-        
-        
-        
-        
+        [canvas.clear() for canvas in self._canvas]
